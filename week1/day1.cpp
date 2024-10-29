b9c416a5abd7e817127f031ce5b1f99eee199f37
peter9217 <peter920107@gmail.com>
2024-10-29 19:43:36 +0900

    20241029/day1/c++개요

---
+++ week1/day1.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //C++표준 라이브러리
+
+using namespace std; //  안붙이면 cout등 라이브러리에 포함된 모든 코드에 std:: 붙어야함.
+int main() { //함수 정의
+    cout << 1+1<<2+3 <<"Hello, World!\n" ; // <<는 and 느낌
+    return 0;
+}
+
