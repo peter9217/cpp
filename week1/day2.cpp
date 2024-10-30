32f80e4a95707149195626b780a79f80fcb44f8e
peter9217 <peter920107@gmail.com>
2024-10-30 18:52:49 +0900

    20241030/day2/c++구성요소

---
+++ week1/day2.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+
+using namespace std; 
+int Function(int a){
+    cout << "Function @@\n";
+    return 2+a;
+};
+int main() { 
+    cout <<"Hello, World!\n"<<"\n" ; 
+    cout <<"Hellodddd\t"<<"dasd\n" ; 
+    cout <<", World!\t"<<"dasd\n" ; 
+    cout <<"!\t" <<"dasd"; 
+    cout <<(double)7000*700<<"\n" << endl ;
+    //- // 한줄 주석
+    /*-  나올 때까지 주석*/
+    int  ab = 0;
+    ab=Function(3);
+    cout << ab;   // 쓰지 않으면 warning이 뜸
+    cout << "end";
+
+    return 0;
+}
+
