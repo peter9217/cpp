623fb7fe6102c81610de6bab5425db0eee3afa94
peter9217 <peter920107@gmail.com>
2024-11-05 18:54:32 +0900

    20241105/day6/클래스

---
+++ week1/day6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+inline string meow();
+class cat {
+    string name;
+    unsigned int age;
+public:
+    cat() {
+        name = "길냥이";
+        age = 0;
+    }
+    cat(string initialName, unsigned int initialAge) {
+        name = initialName;
+        age = initialAge;
+    }
+    ~cat(){};
+    void setName(const string newName){
+        name=newName;
+    }
+    string getName() const {
+        return name;
+    }
+    string meow() const {
+        return "야옹";
+    }
+};
+
+int main(){
+    cat rommy;
+    rommy.setName("로미");
+    cout << rommy.getName();
+    cat semmy("새미",2);
+    cout << semmy.getName();
+    // cout << cat.meow();
+}
